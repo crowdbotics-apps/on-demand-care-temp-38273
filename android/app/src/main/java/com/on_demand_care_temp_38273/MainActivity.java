8aa0bf663a200d56cad4f48f9f210aea33f62eea
Crowdbotics <team@crowdbotics.com>
2023-01-12 14:08:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/on_demand_care_temp_38273/MainActivity.java
@@ -0,0 +1,15 @@
+package com.on_demand_care_temp_38273;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "on_demand_care_temp_38273";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "on-demand-care-temp-38273",
+  url: "https://on-demand-care-temp-38273.botics.co",
+  api: "https://on-demand-care-temp-38273.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
